0961648ac8fda54983b89c3267b8850777243abf
artemdulina <artem.dulina@gmail.com>
2016-11-18 16:17:10 +0300

    add project

---
+++ CustomEchoService/EchoServer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NLog;
+
+namespace CustomEchoService
+{
+    public partial class EchoServer : ServiceBase
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private Task ListnerTask { get; set; }
+        private CancellationTokenSource TokenSource { get; set; }
+        private CancellationToken CancelToken { get; set; }
+
+        public EchoServer()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                Logger.Info("Service launch...");
+                TokenSource = new CancellationTokenSource();
+                CancelToken = TokenSource.Token;
+                Task.Factory.StartNew(ListenerFunction, CancelToken);
+                Logger.Info("EchoService successfully started");
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                TokenSource.Cancel();
+                Logger.Info("EchoService successfully stopped");
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);
+            }
+        }
+
+        private void ListenerFunction()
+        {
+            // Устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // Создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Начинаем слушать соединения
+                while (true)
+                {
+                    // Программа приостанавливается, ожидая входящее соединение
+                    Socket handler = sListener.Accept();
+                    try
+                    {
+                        string data = null;
+
+                        // Мы дождались клиента, пытающегося с нами соединиться
+
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+
+                        data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                        // Показываем данные на консоли                    
+
+                        // Отправляем ответ клиенту
+                        string reply = "Thank you for the request " + data.Length.ToString()
+                                       + " symbols." + "\nYour request message was: " + data;
+                        byte[] msg = Encoding.UTF8.GetBytes(reply);
+                        handler.Send(msg);
+
+                        if (data.IndexOf("<TheEnd>", StringComparison.Ordinal) > -1)
+                        {
+                            break;
+                        }
+                    }
+                    finally
+                    {
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Logger.Error(exception);
+            }
+            finally
+            {
+                sListener.Shutdown(SocketShutdown.Both);
+                sListener.Close();
+            }
+        }
+    }
+}
